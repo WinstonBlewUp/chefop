4ad6e921d5ae0d156f8f12bdce912dff1071708c
WinstonBlewUp <playlist.robin@gmail.com>
2025-05-18 16:56:48 +0200

    ajout du menu de controle de la navbar
    Création de la table pour gérer les pages ajoutées
    
    création d'un composant blade pour gérer les pages à ajouter (suppression  ajout et tutti cuanti)
    séléction dans les pages publiées uniquement

---
+++ app/Models/MenuLink.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MenuLink extends Model
+{
+    protected $fillable = ['page_id'];
+
+    public function page()
+    {
+        return $this->belongsTo(Page::class);
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+@include('dashboard.partials._menu')
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
